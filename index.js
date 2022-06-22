fa961e2f831ce368028f6d29e8594471d7c30094
roman-laptop <roman.calderin.25@gmail.com>
2022-06-21 19:11:31 -0500

    :tada:

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const {SerialPort} = require('serialport');
 const {ReadlineParser} = require('@serialport/parser-readline')
 
 
-const port = new SerialPort({ path: '/COM4', baudRate: 9600 })
+const port = new SerialPort({ path: '/COM3', baudRate: 9600 })
 
   const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
