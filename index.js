7e73da6647b27e7a740bad01127f8d87a4aa767b
roman-laptop <roman.calderin.25@gmail.com>
2022-06-22 07:11:58 -0500

    :rocket:

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const http = require('http')
-const express = require('express') //**express */
-const Socket = require('socket.io')//** socket.io */
+const http = require("http");
+const express = require("express"); //**express */
+const Socket = require("socket.io"); //** socket.io */
 
-const app = express()//**init express */
-const server = http.createServer(app);//**created server
-const io = Socket.listen(server)//**init socket.io *//
+const app = express(); //**init express */
+const server = http.createServer(app); //**created server
+const io = Socket.listen(server); //**init socket.io *//
 
-app.use(express.static(__dirname + '/public'));//** ubication the files public */
-server.listen(process.env.PORT || 3000, () => console.log('server on port 3000'));
+app.use(express.static(__dirname + "/public")); //** ubication the files public */
+server.listen(process.env.PORT || 3000, () => console.log("server on port 3000"));
 
+const { SerialPort } = require("serialport");
+const { ReadlineParser } = require("@serialport/parser-readline");
 
-const {SerialPort} = require('serialport');
-const {ReadlineParser} = require('@serialport/parser-readline')
+const port = new SerialPort({ path: "/COM3", baudRate: process.env.RATE || 9600 });
 
+const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
 
-const port = new SerialPort({ path: '/COM3', baudRate: 9600 })
+parser.on("open", function () {
+    console.log("connection is opened");
+});
 
-  const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
-
-  parser.on('open', function () {
-    console.log('connection is opened');
-  });
-  
-  parser.on('data', function (data) {
+parser.on("data", function (data) {
     let temp = parseInt(data, 10) + " °C";
     console.log(temp);
-    io.emit('temp', data.toString());
-  });
-  
-  parser.on('error', (err) => console.log(err));
-  port.on('error', (err) => console.log(err));
+    io.emit("temp", data.toString());
+});
+
+parser.on("error", (err) => console.log(err));
+port.on("error", (err) => console.log(err));
